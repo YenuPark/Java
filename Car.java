4f082b05a9423b0eae522b832d86b690806eaf02
YenuPark <yenuzzang@naver.com>
2020-11-11 22:27:29 +0900

    CarExample들 필드 파일

--- Car.java
+++ Car.java
@@ -2,9 +2,29 @@ package chapter06;
 
 public class Car {
 	String company = "현대자동차";
-	String model = "그렌저";
-	String color = "검정";
-	int maxSpeed = 350;
+	String model;;
+	String color;
+	int maxSpeed;
 	int speed;
+	
+	
+	Car(){
+		
+	}
+	
+	Car(String model){
+		this.model=model;
+	}
+	
+	Car(String model, String color){
+		this.model=model;
+		this.color=color;
+	}
+	
+	Car(String model, String color, int maxSpeed){
+		this.model=model;
+		this.color=color;
+		this.maxSpeed=maxSpeed;
+	}
 
 }
